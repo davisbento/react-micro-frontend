2c4b521da067ef0913bd1cc612c51ce1bbef8fcf
davisbento <davisilva4222@gmail.com>
2020-12-08 10:27:43 -0300

    bootstrap cart

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const itensNumber = faker.random.number();
-
-document.getElementById('dev-cart').innerHTML = `<div>You have ${itensNumber} items in your cart</div>`;
+import('./bootstrap');
